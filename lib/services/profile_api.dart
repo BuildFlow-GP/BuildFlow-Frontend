4eb20b01e4be3b322fbc743f66793223f3ee9ce8
Walaessam27 <145562141+Walaessam27@users.noreply.github.com>
2025-05-10 16:31:11 +0300

    func of profile api

--- lib/services/profile_api.dart
+++ lib/services/profile_api.dart
@@ -1,37 +1,35 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import '../models/user.dart';
 import 'package:logger/logger.dart';
+import '../models/user.dart';
 
-// Replace this with your API base URL
-const String apiUrl = 'http://localhot:5000/api/profile';
-
+const String apiUrl = 'http://localhost:5000/api/profile';
 final Logger logger = Logger();
 
 Future<User?> fetchUserProfile() async {
   const storage = FlutterSecureStorage();
 
-  // Retrieve token from secure storage
   String? token = await storage.read(key: 'jwt_token');
 
   if (token == null) {
+    logger.w('Token not found in secure storage');
     return null;
   }
 
   try {
     final response = await http.get(
       Uri.parse(apiUrl),
-      headers: {
-        'Authorization': 'Bearer $token', // Add the token to headers
-      },
+      headers: {'Authorization': 'Bearer $token'},
     );
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
+      logger.i('Successfully fetched user profile');
       return User.fromJson(data['user']);
     } else {
-      throw Exception('Failed to load profile');
+      logger.w('Failed to load profile. Status Code: ${response.statusCode}');
+      return null;
     }
   } catch (e) {
     logger.e('Error fetching profile: $e');
