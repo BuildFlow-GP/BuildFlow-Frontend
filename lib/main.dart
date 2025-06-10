4c86cdf450f0d541fede89f7faf5b49067510769
Walaessam27 <145562141+Walaessam27@users.noreply.github.com>
2025-06-10 10:08:10 +0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-
 import 'themes/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'screens/sign/signin_screen.dart';
 import 'package:get/get.dart';
-// import 'widgets/drawer_wrapper.dart';
-// import 'screens/payment_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // مهم قبل async init
@@ -37,7 +34,30 @@ class MyApp extends StatelessWidget {
       ),
 
       home: SignInScreen(),
-
     );
   }
 }
+// import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'dart:developer';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   try {
+//     await Firebase.initializeApp();
+//     print("✅ Firebase Initialized Successfully");
+//   } catch (e) {
+//     print("❌ Firebase Initialization Failed: $e");
+//   }
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Scaffold(body: Center(child: Text('Firebase Check'))),
+//     );
+//   }
+// }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
 import emoji_picker_flutter
 import file_selector_macos
+import firebase_auth
+import firebase_core
+import firebase_storage
 import flutter_secure_storage_macos
 import path_provider_foundation
 import shared_preferences_foundation
@@ -14,8 +18,12 @@ import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
